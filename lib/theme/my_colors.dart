7d96b678759847929ce1c652c9f3d4ba815f236d
Burenbayar <c24io1046@ufe.edu.mn>
2026-05-05 15:25:47 +0800

    test2

--- lib/theme/my_colors.dart
+++ lib/theme/my_colors.dart
@@ -3,17 +3,4 @@ import 'package:flutter/material.dart';
 class MyColors {
   static const primary500 = Color.fromRGBO(19, 10, 120, 0.9);
 
-  static const highlight500 = Color.fromRGBO(246, 207, 52, 0.9);
-
-  static const mycolor400 = Color.fromARGB(255, 186, 7, 189);
-  static const mycolor500 = Color.fromARGB(255, 75, 5, 110);
-
-  static const success400 = Color.fromRGBO(6, 194, 112, 1);
-  static const success300 = Color.fromRGBO(57, 217, 138, 1);
-
-  static const error400 = Color.fromRGBO(255, 59, 59, 1);
-  static const error300 = Color.fromRGBO(255, 92, 92, 1);
-
-  static const dark200 = Color.fromRGBO(143, 144, 166, 1);
-  static const dark100 = Color.fromRGBO(199, 201, 217, 1);
 }
